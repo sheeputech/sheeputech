2296e09219a90d9042b7ba7ce3e0fce4504f3011
Kazumasa <p.m.byzance0817@gmail.com>
2019-04-23 18:18:30 +0900

    frontend: Add components: Layout, Home, NotFound

---
+++ frontend/src/pages/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Here is Layout</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/NotFound/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Here is 404 Not Found</div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>

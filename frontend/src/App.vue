bffdc994ff9d92d92837508ed6b0a5ba1f606b43
Kazumasa <p.m.byzance0817@gmail.com>
2019-04-23 18:16:59 +0900

    frontend: Add root component a transition

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="fade">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
     export default {}
 </script>
+
+<style lang="scss" scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>

1ef2a47e0e0de0080019189d6698226d8ee72029
Kazumasa <p.m.byzance0817@gmail.com>
2019-04-23 18:18:15 +0900

    frontend: Add item to store: device info (isMobile)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,18 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        device: {
+            isMobile: ['iPhone', 'Mobile'].some(userAgent => navigator.userAgent.indexOf(userAgent) > 0)
+        }
+    },
+    getters: {
+        getIsMobile(state) {
+            return state.device.isMobile
+        }
+    },
     mutations: {},
 })
